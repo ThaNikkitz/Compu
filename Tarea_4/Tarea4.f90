5802530d234c6cf44856cc0ea4960af8cc100227
ThaNikkitz <nico.espinozam@gmail.com>
2017-06-15 17:09:12 -0400

    Hay que empezar a trabajar en la Tarea 4

---
+++ Tarea_4/Tarea4.f90
@@ -0,0 +1,12 @@
+program Tarea4
+implicit none
+
+integer :: i, j, k, n
+double precision :: y, m, t, dt, alfa, beta
+double precision, allocatable :: xp(:), yi(:)
+
+Open(unit = 10, file = 'input.txt')
+read(10,*)
+
+
+Close(unit = 10)
