50f646bf2f5b4e98264828002e1b854e62917ea2
Nico Espinoza <nico.espinozam@gmail.com>
2017-07-24 18:25:07 -0400

    Euler Listo

--- Proyecto/Euler_Proyecto.f90
+++ Proyecto/Euler_Proyecto.f90
@@ -2,13 +2,14 @@ Program Euler_Proyecto
 implicit none
 
 integer :: i, j, kk, ptiempo
-integer, parameter :: m = 128, n = 64
+integer, parameter :: m = 64, n = 32
 double precision :: dt, delta, Temp(n,m), Tempk(n,m), Calores(n,m), tiempo, alfa, k, Cp, ro
 k = 100.0d0
 ro = 8862.0d0
-Cp = 421.0d0
+ Cp = 421.0d0
 
 alfa = k/(ro*Cp)
+delta = 2.0d0/m
 
 write(*,*) 'Ingrese tamaño del paso de tiempo, número de pasos'
 read(*,*) dt, tiempo
@@ -19,25 +20,37 @@ Temp = Tempk
 
 Do kk = 1, ptiempo
 	Do i = 1, n
-		Do j = 1, m
-			Tempk(1,:) = 0.0d0
-			Tempk(n,:) = 0.0d0
-			If (j == 1 .and. i < n/2) Then !Mitad izquierda de la primera fila
-				Temp(i,j) = Tempk(i,j) + alfa*(2*Tempk(i,j+1) + Tempk(i+1,j) + Tempk(i-1,j) + 2000*delta/k - 4*Tempk(i,j))*dt/(delta**2)
-			Elseif (j == 1 .and. i > n/2) Then !Mitad derecha de la primera fila
-				Temp(i,j) = Tempk(i,j) + alfa*(2*Tempk(i,j+1) + Tempk(i+1,j) + Tempk(i,j-1) - 4*Tempk(i,j))*dt/(delta**2)
-			Elseif (j == m .and. i < n/2) Then !Mitad izquierda de la última fila
-				Temp(i,j) = Tempk(i,j) + alfa*(2000*delta/k + Tempk(i+1,j) + Tempk(i-1,j) + 2*Tempk(i,j-1) - 4*Tempk(i,j))*dt/(delta**2)
-			Elseif (j == m .and. i > n/2) Then !Mitad derecha de la última fila
-				Temp(i,j) = Tempk(i,j) + alfa*(Tempk(i+1,j) + Tempk(i-1,j) + 2*Tempk(i,j-1) - 4*Tempk(i,j))*dt/(delta**2)
+		Do j = 2, m-1
+			If (i == 1 .and. j < m/2) Then !Mitad izquierda de la primera fila
+				Temp(i,j) = Tempk(i,j) + alfa*(2*Tempk(i+1,j) + Tempk(i,j+1) + Tempk(i,j-1) + 200000*delta/k - 4*Tempk(i,j))*dt/(delta**2)
+
+			Elseif (i == 1 .and. j >= m/2) Then !Mitad derecha de la primera fila
+				Temp(i,j) = Tempk(i,j) + alfa*(2*Tempk(i+1,j) + Tempk(i,j+1) + Tempk(i,j-1) - 4*Tempk(i,j))*dt/(delta**2)
+
+			Elseif (i == n .and. j < m/2) Then !Mitad izquierda de la última fila
+				Temp(i,j) = Tempk(i,j) + alfa*(200000*delta/k + Tempk(i,j+1) + Tempk(i,j-1) + 2*Tempk(i-1,j) - 4*Tempk(i,j))*dt/(delta**2)
+
+			Elseif (i == n .and. j >= m/2) Then !Mitad derecha de la última fila
+				Temp(i,j) = Tempk(i,j) + alfa*(Tempk(i,j+1) + Tempk(i,j-1) + 2*Tempk(i-1,j) - 4*Tempk(i,j))*dt/(delta**2)
+
 			Else !Nodos internos
 				Temp(i,j) = Tempk(i,j) + alfa*(Tempk(i,j+1) + Tempk(i+1,j) + Tempk(i-1,j) + Tempk(i,j-1) - 4*Tempk(i,j))*dt/(delta**2)
 			End If
 		End Do
+		Temp(:,1) = 0.0d0
+		Temp(:,m) = 0.0d0
 	End Do
 	Tempk = Temp
 End Do
 
-write(*,*) Temp
+!write(*,*) Temp(1,1)
+
+Open(Unit = 10, file = 'Blah.txt')
+Do i = 1, n
+	write(10,*) Temp(i,:)
+End Do
+ Close(Unit = 10)
+
+!write(*,*) Temp
 
 End Program
