659f4db60597a02e0e37db4e3b0dbcedbeaf49f8
Nico Espinoza <nico.espinozam@gmail.com>
2017-07-25 16:25:59 -0400

    Cambio en el nodo n/2 en el metodo de Euler

--- Proyecto/Euler_Proyecto.f90
+++ Proyecto/Euler_Proyecto.f90
@@ -2,7 +2,7 @@ Program Euler_Proyecto
 implicit none
 
 integer :: i, j, kk, ptiempo
-integer, parameter :: m = 64, n = 32
+integer, parameter :: m = 128, n = 64
 double precision :: dt, delta, Temp(n,m), Tempk(n,m), Calores(n,m), tiempo, alfa, k, Cp, ro
 k = 100.0d0
 ro = 8862.0d0
@@ -21,16 +21,16 @@ Temp = Tempk
 Do kk = 1, ptiempo
 	Do i = 1, n
 		Do j = 2, m-1
-			If (i == 1 .and. j < m/2) Then !Mitad izquierda de la primera fila
+			If (i == 1 .and. j <= m/2) Then !Mitad izquierda de la primera fila
 				Temp(i,j) = Tempk(i,j) + alfa*(2*Tempk(i+1,j) + Tempk(i,j+1) + Tempk(i,j-1) + 200000*delta/k - 4*Tempk(i,j))*dt/(delta**2)
 
-			Elseif (i == 1 .and. j >= m/2) Then !Mitad derecha de la primera fila
+			Elseif (i == 1 .and. j > m/2) Then !Mitad derecha de la primera fila
 				Temp(i,j) = Tempk(i,j) + alfa*(2*Tempk(i+1,j) + Tempk(i,j+1) + Tempk(i,j-1) - 4*Tempk(i,j))*dt/(delta**2)
 
-			Elseif (i == n .and. j < m/2) Then !Mitad izquierda de la última fila
+			Elseif (i == n .and. j <= m/2) Then !Mitad izquierda de la última fila
 				Temp(i,j) = Tempk(i,j) + alfa*(200000*delta/k + Tempk(i,j+1) + Tempk(i,j-1) + 2*Tempk(i-1,j) - 4*Tempk(i,j))*dt/(delta**2)
 
-			Elseif (i == n .and. j >= m/2) Then !Mitad derecha de la última fila
+			Elseif (i == n .and. j > m/2) Then !Mitad derecha de la última fila
 				Temp(i,j) = Tempk(i,j) + alfa*(Tempk(i,j+1) + Tempk(i,j-1) + 2*Tempk(i-1,j) - 4*Tempk(i,j))*dt/(delta**2)
 
 			Else !Nodos internos
