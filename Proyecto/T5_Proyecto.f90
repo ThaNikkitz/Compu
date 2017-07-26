97acaf31593281731c402892a827a76ee6038fe3
Nico Espinoza <nico.espinozam@gmail.com>
2017-07-26 01:38:04 -0400

    Listo la adaptacion de la tarea 5 al proyecto! YAYYY!

--- Proyecto/T5_Proyecto.f90
+++ Proyecto/T5_Proyecto.f90
@@ -15,16 +15,14 @@ ro = 8862.0d0
 alpha = kappa/(ro*Cp)
 q = 1.0d5
 
-allocate(qx(1:n,1:m), qy(1:n,1:m), T(1:n*m), A(1:n*m,1:n*m), b(1:n*m), Temperaturas(1:n+2,1:m+2), Calores(1:n,1:m))
+allocate(qx(1:n,1:m), qy(1:n,1:m), T(1:n*m), A(1:n*m,1:n*m), b(1:n*m), Temperaturas(1:m,1:n+2), Calores(1:n,1:m))
 
 Do i = 1,m
 	Do j = 1,n
 		If (i == 1 .and. j <= n/2) Then
 			b((j-1)*m+i) = -2.0d0*delta*q/kappa
-			write(*,*) j, i
 		Elseif (i == m .and. j <= n/2) Then
 			b((j-1)*m+i) = -2.0d0*delta*q/kappa
-			write(*,*) j, i
 		Else
 			b((j-1)*m+i) = 0.0d0
 		End If
@@ -90,65 +88,33 @@ write(13,*)
 write(13,*) b
  Close(Unit = 13)
 
-!Call Gseid(A, b, n, T, imax, es, lambda)
+Call Gseid(A, b, n, m, T, imax, es, lambda)
 
-!Do i = 2,n+1
-!	Temperaturas(i,:) = T((i-2)*n+1:(i-1)*n)
-!End Do
+Do j = 2,n+1
+	Temperaturas(:,j) = T((j-2)*m+1:(j-1)*m)
+End Do
 
-!Temperaturas(1,:) = 0
-!Temperaturas(n,:) = 0
+!write(*,*) Temperaturas(1,n+2)
+
+!Temperaturas(:,1) = 0.0d0
+!Temperaturas(:,n+2) = 0.0d0
+
+!write(*,*) 'Todo bien por aca'
 
 !qx(:,2:n) = -kappa*(Temperaturas(2:n+1,3:n+1) - Temperaturas(2:n+1,1:n))/(2*delta)
 !qy(1:n,:) = -kappa*(Temperaturas(1:n,1:n) - Temperaturas(3:n+1,1:n))/(2*delta)
 !qy(:,1) = 0
 !qy(n,:) = 0
 
-
-!Open(Unit = 12, file = 'ouput1.txt')
-!Do l = 1,n+1
-!	write(12,"(15F10.5)") Temperaturas(l,:)
-!End Do
-! Close(Unit = 12)
-
-
 01	format(1x, 1p16e15.7)
 
-!Call Gseid(A, b, n, T, imax, es, lambda)
+Open(Unit = 12, file = 'output.txt')
+Do l = 1,m
+	write(12,01) Temperaturas(l,:)
+End Do
+ Close(Unit = 12)
 
-!Do i = 2,n+1
-	!Temperaturas(i,:) = T((i-2)*n+1:(i-1)*n)
-!End Do
-!Temperaturas(1,:) = 100
-!Temperaturas(:,n+1) = 50
-!Temperaturas(1,n+1) = 150
-
-
-!qx(:,2:n) = -kappa*(Temperaturas(2:n+1,3:n+1) - Temperaturas(2:n+1,1:n))/(2*delta)
-!qy(1:n,:) = -kappa*(Temperaturas(1:n,1:n) - Temperaturas(3:n+1,1:n))/(2*delta)
-!qx(:,1) = 0
-!qy(n,:) = 0
-
-!Do i = 1, n+1
-		!write(*,01) Temperaturas(i,:)
-!End Do
-
-!write(*,*) Temperaturas(5,1)
-
-!Open(Unit = 14, file = 'Calor.txt')
-!write(14,*) 'Componentes en x del flujo de calor'
-!Do k = 1,n
-!	write(14,01) qx(k,:)
-!End Do
-!write(14,*) 'Componentes en y del flujo de calor'
-!Do l = 1,n
-!	write(14,01) qy(l,:)
-!End Do
-!write(14,*) 'Magnitud del flujo de calor'
-!Do m = 1,n
-!	write(14,01) Sqrt(qx(m,:)**2 + qy(m,:)**2)
-!End Do
- Close(Unit = 14)
+!Acá borré mucho, ojo :P
 
 deallocate(qx, qy, T, A, b, Calores, Temperaturas)
 
@@ -156,28 +122,28 @@ end program
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-subroutine Gseid(a, b, n, x, imax, es, lambda)
+subroutine Gseid(a, b, n, m, x, imax, es, lambda)
 
 	implicit none
 
-	integer :: ii, jj, n, iter, centinela, imax
-	double precision :: a(n**2, n**2), b(n**2), x(n**2), dummy, suma, es, ea, lambda, old
+	integer :: ii, jj, n, m, iter, centinela, imax
+	double precision :: a(n*m, n*m), b(n*m), x(n*m), dummy, suma, es, ea, lambda, old
 
 	es = 1.0d-10
 	lambda = 0.01
 	imax = 1000000
 
-	Do ii = 1, n**2
+	Do ii = 1,n*m
 		dummy = a(ii,ii)
-		Do jj = 1, n**2
+		Do jj = 1, n*m
 			a(ii,jj) = a(ii,jj)/dummy
 		End Do
 		b(ii) = b(ii)/dummy
 	End Do
 
-	Do ii = 1, n**2
+	Do ii = 1, n*m
 		suma = b(ii)
-		Do jj = 1, n**2
+		Do jj = 1, n*m
 			If (ii /= jj) Then
 				suma = suma - a(ii,jj)*x(jj)
 			End If
@@ -185,14 +151,16 @@ subroutine Gseid(a, b, n, x, imax, es, lambda)
 		x(ii) = suma
 	End Do
 
+write(*,*) 'checkpoint!'
+
 	iter = 1
 	centinela = 0
 	Do While(centinela /= 1 .and. iter < imax)
 		centinela = 1
-		Do ii = 1, n**2
+		Do ii = 1, n*m
 			old = x(ii)
 			suma = b(ii)
-			Do jj = 1,n**2
+			Do jj = 1,n*m
 				If (ii /= jj) Then
 					suma = suma - a(ii,jj)*x(jj)
 				End If
@@ -206,8 +174,18 @@ subroutine Gseid(a, b, n, x, imax, es, lambda)
 			End If
 		End Do
 		iter = iter + 1
+		If (Mod(iter,1500) == 0) Then
+			write(*,*) 'Iteracion número: ', iter
+		End If
+		If (Mod(iter,300000) == 1) Then
+			write(*,*) 'Anda a tomarte un tecito, le falta harto'
+			write(*,*) 'Para que te hagas una idea, el error va en: ', ea
+		End If
+		If (Mod(iter,400000) == 1) Then
+			write(*,*) 'Y tu pc ya empieza a oler a quemado...'
+		End If
 	End Do
-!	write(*,*) iter
+	write(*,*) iter
 End Subroutine
 
 
